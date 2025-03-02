0f7117e2befde3bcc910e6e9e6c233bfdb9d251a
0d41146 <41146f@gmail.com>
2025-03-03 08:52:10 +0900

    Update script

--- scripts/build.tcl
+++ scripts/build.tcl
@@ -5,6 +5,8 @@ set src_files [list $top_dir/config.vh $top_dir/proc.v $top_dir/cfu.v $top_dir/m
 set nproc [exec nproc]
 
 create_project -force $proj_name $top_dir/vivado -part $part_name
+set_property strategy Flow_PerfOptimized_high [get_runs synth_1]
+set_property strategy Performance_ExplorePostRoutePhysOpt [get_runs impl_1]
 add_files -norecurse $src_files
 add_files -fileset constrs_1 -norecurse $top_dir/main.xdc
 update_compile_order -fileset sources_1
@@ -17,13 +19,6 @@ export_ip_user_files -of_objects [get_files $top_dir/vivado/$proj_name.srcs/sour
 create_ip_run [get_files -of_objects [get_fileset sources_1] $top_dir/vivado/$proj_name.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0.xci]
 launch_runs clk_wiz_0_synth_1 -jobs $nproc
 wait_on_run clk_wiz_0_synth_1
-
-set_property STEPS.OPT_DESIGN.ARGS.DIRECTIVE ExploreWithRemap [get_runs impl_1]
-set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE Auto_1 [get_runs impl_1]
-set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE ExploreWithAggressiveHoldFix [get_runs impl_1]
-set_property STEPS.ROUTE_DESIGN.ARGS.DIRECTIVE AggressiveExplore [get_runs impl_1]
-set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
-set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.DIRECTIVE ExploreWithAggressiveHoldFix [get_runs impl_1]
 
 launch_runs impl_1 -to_step write_bitstream -jobs $nproc
 wait_on_run impl_1

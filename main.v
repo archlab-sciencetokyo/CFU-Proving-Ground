2706cf84a1f2623e44e999e0b93780a83f56e8c2
Fujino <166023465+0d41146@users.noreply.github.com>
2025-05-01 20:58:48 +0900

    Update main.v

--- main.v
+++ main.v
@@ -1,35 +1,37 @@
-/* CFU Proving Ground since 2025-02    Copyright(c) 2025 Archlab. Science Tokyo /
-/ Released under the MIT license https://opensource.org/licenses/mit           */
-
+/******************************************************************************************/
+/* CFU Proving Ground since 2025-02              Copyright(c) 2025 Archlab. Science Tokyo */
+/* Released under the MIT license https://opensource.org/licenses/mit                     */
+/******************************************************************************************/
 `resetall
 `default_nettype none
-
 `include "config.vh"
 
+/******************************************************************************************/
 module main (
     input  wire clk_i       ,
-    output wire st7789_SDA  ,
-    output wire st7789_SCL  ,
-    output wire st7789_DC   , 
-    output wire st7789_RES 
+    output reg  st7789_SDA  ,
+    output reg  st7789_SCL  ,
+    output reg  st7789_DC   , 
+    output reg  st7789_RES 
 );
-//    wire rst_ni = 1;
-    reg rst_ni = 0; initial #15 rst_ni = 1;
     wire clk, locked;
 
 `ifdef SYNTHESIS
     clk_wiz_0 clk_wiz_0 (
         .clk_out1           (clk                ), // output clk_out1
-        .reset              (!rst_ni            ), // input reset
+        .reset              (!rst_in            ), // input reset
         .locked             (locked             ), // output locked
         .clk_in1            (clk_i              )  // input clk_in1
     );
+    reg rst_in=0;
 `else
-    assign clk      = clk_i ;
-    assign locked   = 1'b1  ;
+    assign clk = clk_i;
+    assign locked = 1;
+    reg rst_in = 1; initial #15 rst_in = 0;
 `endif
 
-    wire                        rst             = !rst_ni || !locked;
+    wire rst = rst_in || !locked;
+    
     wire [`IBUS_ADDR_WIDTH-1:0] imem_raddr  ;
     wire [`IBUS_DATA_WIDTH-1:0] imem_rdata  ;
     wire                        dbus_we     ;
@@ -38,48 +40,64 @@ module main (
     wire [`DBUS_STRB_WIDTH-1:0] dbus_wstrb  ;
     wire [`DBUS_DATA_WIDTH-1:0] dbus_rdata  ;
 
+`ifdef PERFCNTR ///// enable performance counter
     reg rdata_sel = 0; always @(posedge clk) rdata_sel <= dbus_addr[30];
     assign dbus_rdata = (rdata_sel) ? perf_rdata : dmem_rdata;
+`else
+    assign dbus_rdata = dmem_rdata;
+`endif
 
+    /********************************************************************************/
     cpu cpu (
-        .clk_i              (clk                ), // input  wire
-        .rst_i              (rst                ), // input  wire
-        .ibus_araddr_o      (imem_raddr         ), // output wire [`IBUS_ADDR_WIDTH-1:0]
-        .ibus_rdata_i       (imem_rdata         ), // input  wire [`IBUS_DATA_WIDTH-1:0]
-        .dbus_addr_o        (dbus_addr          ), // output wire [`DBUS_ADDR_WIDTH-1:0]
-        .dbus_wvalid_o      (dbus_we            ), // output wire
-        .dbus_wdata_o       (dbus_wdata         ), // output wire [`DBUS_DATA_WIDTH-1:0]
-        .dbus_wstrb_o       (dbus_wstrb         ), // output wire [`DBUS_STRB_WIDTH-1:0]
-        .dbus_rdata_i       (dbus_rdata         )  // input  wire [`DBUS_DATA_WIDTH-1:0]
+        .clk_i              (clk            ), // input  wire
+        .rst_i              (rst            ), // input  wire
+        .ibus_araddr_o      (imem_raddr     ), // output wire [`IBUS_ADDR_WIDTH-1:0]
+        .ibus_rdata_i       (imem_rdata     ), // input  wire [`IBUS_DATA_WIDTH-1:0]
+        .dbus_addr_o        (dbus_addr      ), // output wire [`DBUS_ADDR_WIDTH-1:0]
+        .dbus_wvalid_o      (dbus_we        ), // output wire
+        .dbus_wdata_o       (dbus_wdata     ), // output wire [`DBUS_DATA_WIDTH-1:0]
+        .dbus_wstrb_o       (dbus_wstrb     ), // output wire [`DBUS_STRB_WIDTH-1:0]
+        .dbus_rdata_i       (dbus_rdata     )  // input  wire [`DBUS_DATA_WIDTH-1:0]
     );
 
     imem #(
-        .RAM_SIZE           (`IMEM_SIZE         )
+        .RAM_SIZE           (`IMEM_SIZE     )
     ) imem (
-        .clk_i              (clk                ), // input  wire
-        .raddr_i            (imem_raddr         ), // input  wire [ADDR_WIDTH-1:0]
-        .rdata_o            (imem_rdata         )  // output reg  [DATA_WIDTH-1:0]
+        .clk_i              (clk            ), // input  wire
+        .raddr_i            (imem_raddr     ), // input  wire [ADDR_WIDTH-1:0]
+        .rdata_o            (imem_rdata     )  // output reg  [DATA_WIDTH-1:0]
     );
 
-    wire dmem_we = dbus_we & (dbus_addr[28]);
-    wire [31:0] dmem_addr = dbus_addr;
-    wire [31:0] dmem_wdata = dbus_wdata;
-    wire [3:0] dmem_wstrb = dbus_wstrb;
+    wire        dmem_we = dbus_we & (dbus_addr[28]);
     wire [31:0] dmem_rdata;
     dmem #(
-        .RAM_SIZE           (`DMEM_SIZE         )
+        .RAM_SIZE           (`DMEM_SIZE     )
     ) dmem (
-        .clk_i              (clk                ),   // input  wire
-        .we_i               (dmem_we            ),   // input  wire                  
-        .addr_i             (dmem_addr          ),   // input  wire [ADDR_WIDTH-1:0] 
-        .wdata_i            (dmem_wdata         ),   // input  wire [DATA_WIDTH-1:0] 
-        .wstrb_i            (dmem_wstrb         ),   // input  wire [STRB_WIDTH-1:0] 
-        .rdata_o            (dmem_rdata         )    // output reg  [DATA_WIDTH-1:0] 
+        .clk_i              (clk            ), // input  wire
+        .we_i               (dmem_we        ), // input  wire
+        .addr_i             (dbus_addr      ), // input  wire [ADDR_WIDTH-1:0]
+        .wdata_i            (dbus_wdata     ), // input  wire [DATA_WIDTH-1:0]
+        .wstrb_i            (dbus_wstrb     ), // input  wire [STRB_WIDTH-1:0]
+        .rdata_o            (dmem_rdata     )  // output reg  [DATA_WIDTH-1:0]
     );
 
-    wire vmem_we = dbus_we & (dbus_addr[29]);
-    wire [15:0] vmem_addr =  dbus_addr[15:0];
-    wire [ 2:0] vmem_wdata = dbus_wdata[2:0];
+    /********************************************************************************/
+    reg                         r_dbus_we     ;
+    reg  [`DBUS_ADDR_WIDTH-1:0] r_dbus_addr   ;
+    reg  [`DBUS_DATA_WIDTH-1:0] r_dbus_wdata  ;
+    reg  [`DBUS_STRB_WIDTH-1:0] r_dbus_wstrb  ;
+    reg  [`DBUS_DATA_WIDTH-1:0] r_dbus_rdata  ;
+    always @(posedge clk) begin
+        r_dbus_we    <= dbus_we;
+        r_dbus_addr  <= dbus_addr;
+        r_dbus_wdata <= dbus_wdata;
+        r_dbus_wstrb <= dbus_wstrb;
+        r_dbus_rdata <= dbus_rdata;
+    end    
+    
+    wire vmem_we = r_dbus_we & (r_dbus_addr[29]);
+    wire [15:0] vmem_addr =  r_dbus_addr[15:0];
+    wire [ 2:0] vmem_wdata = r_dbus_wdata[2:0];
     wire [15:0] vmem_raddr;
     wire [ 2:0] vmem_rdata_t;
     vmem vmem (
@@ -91,9 +109,9 @@ module main (
         .rdata_o            (vmem_rdata_t       )  // output wire [15:0]
     );
 
-    wire perf_we = dbus_we & (dbus_addr[30]);
-    wire [3:0] perf_addr = dbus_addr[3:0];
-    wire [2:0] perf_wdata = dbus_wdata[2:0];
+    wire perf_we = r_dbus_we & (r_dbus_addr[30]);
+    wire [3:0] perf_addr  = r_dbus_addr[3:0];
+    wire [2:0] perf_wdata = r_dbus_wdata[2:0];
     wire [31:0] perf_rdata;
     perf_cntr perf(
         .clk_i              (clk                ), // input  wire
@@ -103,19 +121,31 @@ module main (
         .rdata_o            (perf_rdata         )  // output wire [31:0]
     );
 
-    wire [15:0] vmem_rdata = {{5{vmem_rdata_t[2]}}, {6{vmem_rdata_t[1]}}, {5{vmem_rdata_t[0]}}};
+    wire st7789_SDA_t;
+    wire st7789_SCL_t;
+    wire st7789_DC_t;
+    wire st7789_RES_t; 
+    wire [15:0] vmem_rdata = {{5{vmem_rdata_t[2]}}, 
+                              {6{vmem_rdata_t[1]}}, {5{vmem_rdata_t[0]}}};
     m_st7789_disp st7789_disp (
         .w_clk              (clk                ), // input  wire
-        .st7789_SDA         (st7789_SDA         ), // output wire
-        .st7789_SCL         (st7789_SCL         ), // output wire
-        .st7789_DC          (st7789_DC          ), // output wire
-        .st7789_RES         (st7789_RES         ), // output wire
+        .st7789_SDA         (st7789_SDA_t       ), // output wire
+        .st7789_SCL         (st7789_SCL_t       ), // output wire
+        .st7789_DC          (st7789_DC_t        ), // output wire
+        .st7789_RES         (st7789_RES_t       ), // output wire
         .w_raddr            (vmem_raddr         ), // output wire [15:0]
         .w_rdata            (vmem_rdata         )  // input  wire [15:0]
     );
-
+    always @(posedge clk) begin
+        st7789_SDA <= st7789_SDA_t;
+        st7789_SCL <= st7789_SCL_t;
+        st7789_DC  <= st7789_DC_t;
+        st7789_RES <= st7789_RES_t;
+    end
+    
 endmodule
 
+/******************************************************************************************/
 module imem #(
     parameter RAM_SIZE      = 4*1024        ,
     parameter ADDR_WIDTH    = 32            ,
@@ -131,7 +161,8 @@ module imem #(
     (* ram_style = "block" *) reg [DATA_WIDTH-1:0] imem [0:(2**VALID_ADDR_WIDTH)-1];
     `include "memi.txt"
 
-    wire [VALID_ADDR_WIDTH-1:0] valid_raddr = raddr_i[VALID_ADDR_WIDTH+OFFSET_WIDTH-1:OFFSET_WIDTH];
+    wire [VALID_ADDR_WIDTH-1:0] valid_raddr = 
+                                raddr_i[VALID_ADDR_WIDTH+OFFSET_WIDTH-1:OFFSET_WIDTH];
 
     always @(posedge clk_i) begin
         rdata_o <= imem[valid_raddr];
@@ -139,6 +170,7 @@ module imem #(
 
 endmodule
 
+/******************************************************************************************/
 module dmem #(
     parameter RAM_SIZE      = 4*1024        ,
     parameter ADDR_WIDTH    = 32            ,
@@ -158,7 +190,8 @@ module dmem #(
     (* ram_style = "block" *) reg [DATA_WIDTH-1:0] dmem [0:(2**VALID_ADDR_WIDTH)-1];
     `include "memd.txt"
 
-    wire [VALID_ADDR_WIDTH-1:0] valid_addr = addr_i[VALID_ADDR_WIDTH+OFFSET_WIDTH-1:OFFSET_WIDTH];
+    wire [VALID_ADDR_WIDTH-1:0] valid_addr = 
+                                addr_i[VALID_ADDR_WIDTH+OFFSET_WIDTH-1:OFFSET_WIDTH];
 
     integer i;
     reg [31:0] rdata = 0;
@@ -176,6 +209,7 @@ module dmem #(
 
 endmodule
 
+/******************************************************************************************/
 module perf_cntr (
     input   wire        clk_i,
     input   wire [3:0]  addr_i,
@@ -200,11 +234,12 @@ module perf_cntr (
     assign rdata_o = rdata;
 endmodule
 
+/******************************************************************************************/
 module vmem (
     input wire clk_i,
     input wire        we_i,
     input wire [15:0] waddr_i,
-    input wire [2:0] wdata_i,
+    input wire  [2:0] wdata_i,
     input wire [15:0] raddr_i,
     output wire [2:0] rdata_o
 );
@@ -270,6 +305,7 @@ module vmem (
 
 endmodule
 
+/******************************************************************************************/
 module m_st7789_disp(
     input  wire w_clk, // main clock signal (100MHz)
     output wire st7789_SDA,
@@ -306,8 +342,9 @@ module m_st7789_disp(
     
     always @(posedge w_clk) if(r_en && !init_done) r_state <= r_state  + 1;
     
+    ///// 11 + 240x240*2 = 11 + 115200 = 115211
     always @(posedge w_clk) if(r_en &&  init_done) begin
-        r_state2 <= (r_state2==115210) ? 0 : r_state2 + 1; // 11 + 240x240*2 = 11 + 115200 = 115211
+        r_state2 <= (r_state2==115210) ? 0 : r_state2 + 1;
     end
 
     reg [7:0] r_x = 0;
@@ -321,7 +358,7 @@ module m_st7789_disp(
     wire [7:0] w_ny = 239-r_y;  
     assign w_raddr = (`LCD_ROTATE==0) ? {r_y, r_x} :  // default
                      (`LCD_ROTATE==1) ? {r_x, w_ny} : // 90 degree rotation
-                     (`LCD_ROTATE==2) ? {w_ny, w_nx} : {w_nx, r_y} ; //180 degree, 240 degree rotation
+                     (`LCD_ROTATE==2) ? {w_ny, w_nx} : {w_nx, r_y} ; //180, 240 deg.rot.
     
     reg  [15:0] r_color = 0;
     always @(posedge w_clk) r_color <= w_rdata;  
@@ -413,6 +450,4 @@ module m_spi(
     assign busy = (r_state!=0 || en);
 endmodule
 /*********************************************************************************************/
-
-
 `resetall

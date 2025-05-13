b2219993b2f54b2cd6b5fa12fe97fdd2b9889ae5
0d41146 <41146f@gmail.com>
2025-05-13 16:07:00 +0900

    Update HLS Template

--- cfu.c
+++ cfu.c
@@ -1,24 +1,11 @@
 #include <stdbool.h>
 
 void cfu(
-    bool   clk_i   ,
-    bool   en_i    ,
     char   funct3_i,
     char   funct7_i,
     int    src1_i  ,
     int    src2_i  ,
-    bool*  stall_o ,
     int*   rslt_o  ) {
 
-#pragma HLS INTERFACE ap_ctrl_none port=return
-#pragma HLS INTERFACE ap_none port=stall_o
-#pragma HLS INTERFACE ap_none port=rslt_o
-
-    *stall_o = 0;
-
-    if (en_i) {
-        *rslt_o = src1_i | src2_i;
-    } else {
-        *rslt_o = 0;
-    }
+    *rslt_o = src1_i | src2_i;
 }

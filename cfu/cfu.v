507fa0ca14a3a4b42b48d4dd60727e09cec6ae65
0d41146 <41146f@gmail.com>
2025-05-13 16:10:50 +0900

    Update CFU Interface

---
+++ cfu/cfu.v
@@ -0,0 +1,20 @@
+module cfu (
+    input  wire        ap_clk  ,
+    input  wire        ap_start,
+    output wire        ap_done ,
+    output wire        ap_idle ,
+    output wire        ap_ready,
+    input  wire [2:0]  funct3_i,
+    input  wire [6:0]  funct7_i,
+    input  wire [31:0] src1_i  ,
+    input  wire [31:0] src2_i  ,
+    input  wire [31:0] rslt_o
+);
+
+    assign ap_done = ap_start;
+    assign ap_idle = 1'b1;
+    assign ap_ready = ap_start;
+
+    assign rslt_o = src1_i | src2_i;
+
+endmodule

c175347862e0616380986961e8d68cc94830762c
0d41146 <41146f@gmail.com>
2025-10-08 16:34:37 +0900

    Fix volatile type usage in performance functions for correct memory access

--- app/perf.c
+++ app/perf.c
@@ -2,19 +2,19 @@
 / Released under the MIT license https://opensource.org/licenses/mit           */
 
 unsigned long long pg_perf_cycle(void) {
-    unsigned int cycle =  *(int *)0x40000004;
-    unsigned int cycleh = *(int *)0x40000008;
+    unsigned int cycle =  *(volatile unsigned int *)0x40000004;
+    unsigned int cycleh = *(volatile unsigned int *)0x40000008;
     return ((unsigned long long)cycleh << 32) | cycle;
 }
 
 void pg_perf_reset(void) {
-    *(char *)0x40000000 = 0;
+    *(volatile char *)0x40000000 = 0;
 }
 
 void pg_perf_enable(void) {
-    *(char *)0x40000000 = 1;
+    *(volatile char *)0x40000000 = 1;
 }
 
 void pg_perf_disable(void) {
-    *(char *)0x40000000 = 2;
+    *(volatile char *)0x40000000 = 2;
 }

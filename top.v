c6a78c307d0f1117d4b19a1711db60652bb2768b
0d41146 <41146f@gmail.com>
2025-10-20 11:34:34 +0900

    Refactor performance counter logic and add trace dump functionality

--- top.v
+++ top.v
@@ -6,22 +6,20 @@
 module top;
     reg clk   = 1; always #5 clk <= ~clk;
     reg rst_n = 0; initial #50 rst_n = 1;
-    reg rxd   = 1;
-    wire txd;
 
 //==============================================================================
 // Perfomance Counter
 //------------------------------------------------------------------------------
     reg [63:0] mcycle = 0;
+    always @(posedge clk) if (!m0.rst && !cpu_sim_fini) mcycle <= mcycle + 1;
+
     reg [63:0] minstret = 0;
     reg [63:0] br_pred_cntr = 0;
     reg [63:0] br_misp_cntr = 0;
-    always @(posedge clk) begin
-        if (!m0.rst && !cpu_sim_fini) mcycle <= mcycle + 1;
-        if (!m0.rst && !cpu_sim_fini && !m0.cpu.stall && !m0.cpu.stall_i && m0.cpu.ExMa_v) minstret <= minstret + 1;
-        if (!m0.rst && !cpu_sim_fini && m0.cpu.ExMa_is_ctrl_tsfr && !m0.cpu.stall_i) br_pred_cntr <= br_pred_cntr + 1;
-        if (!m0.rst && !cpu_sim_fini && m0.cpu.ExMa_is_ctrl_tsfr && m0.cpu.Ma_br_misp && !m0.cpu.stall_i)
-            br_misp_cntr <= br_misp_cntr + 1;
+    always @(posedge clk) if (!m0.rst && !cpu_sim_fini && !m0.cpu.stall_i) begin
+        if (!m0.cpu.stall && m0.cpu.ExMa_v) minstret <= minstret + 1;
+        if (m0.cpu.ExMa_is_ctrl_tsfr) br_pred_cntr <= br_pred_cntr + 1;
+        if (m0.cpu.ExMa_is_ctrl_tsfr && m0.cpu.Ma_br_misp) br_misp_cntr <= br_misp_cntr + 1;
     end
 
 //==============================================================================
@@ -55,6 +53,30 @@ module top;
         $write("===> Total number of branch mispredictions  : %10d\n", br_misp_cntr);
         $write("===> simulation finish!!\n");
         $write("\n");
+    end
+
+//==============================================================================
+// Trace Dump
+//------------------------------------------------------------------------------
+    integer i, j, fp;
+    initial begin
+        fp=$fopen("trace.txt","w");
+        if(fp == 0)begin
+            $display("File_Open_Error_!!!!!");
+            $finish();
+        end
+    end
+    always @(posedge clk) if (minstret < 32'h10000)begin
+        if (!m0.rst && !cpu_sim_fini && !m0.cpu.stall && m0.cpu.MaWb_v && !m0.cpu.stall_i) begin
+            $fwrite(fp, "%08d %08x %08x\n", minstret, m0.cpu.MaWb_pc, m0.cpu.MaWb_ir);
+            for(i=0;i<4;i=i+1) begin
+                for(j=0;j<8;j=j+1) begin
+                    $fwrite(fp, "%08x",m0.cpu.xreg.ram[i*8+j]);
+                    if(j==7)    $fwrite(fp, "\n");
+                    else        $fwrite(fp, " ");
+                end
+            end
+        end
     end
 
     wire sda, scl, dc, res;

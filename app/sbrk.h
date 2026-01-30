286609458ee0b99791a069dc90ce28a15abdd359
Damin3927 <d@damin.dev>
2026-01-30 14:30:19 +0900

    feat: add sbrk and heap section for dynamic memory allocation

---
+++ app/sbrk.h
@@ -0,0 +1,9 @@
+/* CFU Proving Ground since 2025-02    Copyright(c) 2025 Archlab. Science Tokyo /
+/ Released under the MIT license https://opensource.org/licenses/mit           */
+
+#ifndef SBRK_H
+#define SBRK_H
+
+void *_sbrk(int incr);
+
+#endif
